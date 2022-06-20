4a4b2082317bb68b783dc9dee9c8180c197ce9b1
Dexter Castor Döpping <dexter.c.dopping@gmail.com>
2022-06-21 00:24:55 +0200

    Expose ImGui symbols in the dll
    For mods that use C++ and want to use ImGui.

---
+++ src/include/noita_imgui/imgui_config.hpp
@@ -0,0 +1,9 @@
+#ifndef NOITA_IMGUI_IMGUI_CONFIG_HPP
+#define NOITA_IMGUI_IMGUI_CONFIG_HPP
+
+#include <noita_dear_imgui_export.h>
+
+// Export ImGui functionality for any mods that don't use the Lua interface
+#define IMGUI_API NOITA_DEAR_IMGUI_EXPORT
+
+#endif // header guard
