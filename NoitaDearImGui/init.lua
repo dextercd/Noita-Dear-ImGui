e599242ed72fa129bf1674acafe1a63750ba2bf7
Dexter Castor Döpping <dexter.c.dopping@gmail.com>
2022-06-24 17:58:27 +0200

    Fix weird sentence

--- NoitaDearImGui/init.lua
+++ NoitaDearImGui/init.lua
@@ -8,8 +8,9 @@ void* LoadLibraryA(const char*);
 
 ]])
 
--- LuaJIT unloads a library once the `ffi.load` handle is garbage collected,
--- increase the library's reference count does not go below zero.
+-- LuaJIT frees the OS library handle when the `ffi.load` handle is garbage
+-- collected. We need to increase the library's reference count to prevent it
+-- from getting unloaded.
 --
 -- Without this, Noita crashes in JIT-ed Lua code when you use the "New Game"
 -- option, probably because some Lua states are preserved across new runs.
