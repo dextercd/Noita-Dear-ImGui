8a386c8af619c8e4254dc5c34f42037c7efb2dad
Dexter Castor Döpping <dexter.c.dopping@gmail.com>
2022-07-02 15:55:57 +0200

    version_compat_window.hpp header guards and includes

--- src/version_compat_window.hpp
+++ src/version_compat_window.hpp
@@ -1,3 +1,8 @@
+#ifndef NOITA_IMGUI_VERSION_COMPAT_WINDOW_HPP
+#define NOITA_IMGUI_VERSION_COMPAT_WINDOW_HPP
+
+#include <string>
+
 enum incompatibility_reason {
     cant_parse_version,
     version_mismatch,
@@ -5,3 +10,5 @@ enum incompatibility_reason {
 
 void show_version_compat_window();
 void report_incompatibility(std::string mod_name, std::string version, incompatibility_reason reason);
+
+#endif // header guard
