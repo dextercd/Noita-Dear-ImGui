44bb1640caf65ffca033a27397e84443dd1cfd0a
Dexter Castor Döpping <dexter.c.dopping@gmail.com>
2022-09-12 19:25:48 +0200

    Remove ImGui image functions
    Texture related stuff is not supported at the moment.

--- src/lua_features/imgui_widgets_main.cpp
+++ src/lua_features/imgui_widgets_main.cpp
@@ -22,21 +22,6 @@ void add_imgui_widgets_main(sol::table& imgui)
             [](const char* str_id, float size_x, float size_y) { return ImGui::InvisibleButton(str_id, {size_x, size_y}); },
             [](const char* str_id, float size_x, float size_y, ImGuiButtonFlags flags) { return ImGui::InvisibleButton(str_id, {size_x, size_y}, flags); }));
     imgui.set_function("ArrowButton", &ImGui::ArrowButton);
-    imgui.set_function("Image",
-        sol::overload(
-            [](ImTextureID texid, float size_x, float size_y) { return ImGui::Image(texid, {size_x, size_y}); },
-            [](ImTextureID texid, float size_x, float size_y, float uv0_x, float uv0_y) { return ImGui::Image(texid, {size_x, size_y}, {uv0_x, uv0_y}); },
-            [](ImTextureID texid, float size_x, float size_y, float uv0_x, float uv0_y, float uv1_x, float uv1_y) { return ImGui::Image(texid, {size_x, size_y}, {uv0_x, uv0_y}, {uv1_x, uv1_y}); },
-            [](ImTextureID texid, float size_x, float size_y, float uv0_x, float uv0_y, float uv1_x, float uv1_y, float tint_r, float tint_g, float tint_b, float tint_a) { return ImGui::Image(texid, {size_x, size_y}, {uv0_x, uv0_y}, {uv1_x, uv1_y}, {tint_r, tint_g, tint_b, tint_a}); },
-            [](ImTextureID texid, float size_x, float size_y, float uv0_x, float uv0_y, float uv1_x, float uv1_y, float tint_r, float tint_g, float tint_b, float tint_a, float border_r, float border_g, float border_b, float border_a) { return ImGui::Image(texid, {size_x, size_y}, {uv0_x, uv0_y}, {uv1_x, uv1_y}, {tint_r, tint_g, tint_b, tint_a}, {border_r, border_g, border_b, border_a}); }));
-    imgui.set_function("ImageButton",
-        sol::overload(
-            [](ImTextureID texid, float size_x, float size_y) { return ImGui::ImageButton(texid, {size_x, size_y}); },
-            [](ImTextureID texid, float size_x, float size_y, float uv0_x, float uv0_y) { return ImGui::ImageButton(texid, {size_x, size_y}, {uv0_x, uv0_y}); },
-            [](ImTextureID texid, float size_x, float size_y, float uv0_x, float uv0_y, float uv1_x, float uv1_y) { return ImGui::ImageButton(texid, {size_x, size_y}, {uv0_x, uv0_y}, {uv1_x, uv1_y}); },
-            [](ImTextureID texid, float size_x, float size_y, float uv0_x, float uv0_y, float uv1_x, float uv1_y, int frame_padding) { return ImGui::ImageButton(texid, {size_x, size_y}, {uv0_x, uv0_y}, {uv1_x, uv1_y}, frame_padding); },
-            [](ImTextureID texid, float size_x, float size_y, float uv0_x, float uv0_y, float uv1_x, float uv1_y, int frame_padding, float bg_r, float bg_g, float bg_b, float bg_a) { return ImGui::ImageButton(texid, {size_x, size_y}, {uv0_x, uv0_y}, {uv1_x, uv1_y}, frame_padding, {bg_r, bg_g, bg_b, bg_a}); },
-            [](ImTextureID texid, float size_x, float size_y, float uv0_x, float uv0_y, float uv1_x, float uv1_y, int frame_padding, float bg_r, float bg_g, float bg_b, float bg_a, float border_r, float border_g, float border_b, float border_a) { return ImGui::ImageButton(texid, {size_x, size_y}, {uv0_x, uv0_y}, {uv1_x, uv1_y}, frame_padding, {bg_r, bg_g, bg_b, bg_a}, {border_r, border_g, border_b, border_a}); }));
     imgui.set_function("Checkbox", [](const char* label, bool value) { bool changed = ImGui::Checkbox(label, &value); return std::tuple{changed, value}; });
     imgui.set_function("CheckboxFlags",
         [](const char* label, unsigned flags, unsigned flags_value) { auto ret = ImGui::CheckboxFlags(label, &flags, flags_value); return std::tuple{ret, flags}; });
