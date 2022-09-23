4fa1841faa943c2a4235335aa5798a845ee6938f
Dexter Castor Döpping <dexter.c.dopping@gmail.com>
2022-09-23 23:50:34 +0200

    Switch rest of text functions to string_view

--- src/lua_features/imgui_text.cpp
+++ src/lua_features/imgui_text.cpp
@@ -7,10 +7,10 @@
 void add_imgui_text(sol::table& imgui)
 {
     imgui.set_function("TextUnformatted", [](std::string_view text) { ImGui::TextUnformatted(text.data(), text.data() + text.size()); });
-    imgui.set_function("Text", [](const char* text) { ImGui::Text("%s", text); });
-    imgui.set_function("TextColored", [](float colr, float colg, float colb, float cola, const char* text) { ImGui::TextColored({colr, colg, colb, cola}, "%s", text); });
-    imgui.set_function("TextDisabled", [](const char* text) { ImGui::TextDisabled("%s", text); });
-    imgui.set_function("TextWrapped", [](const char* text) { ImGui::TextWrapped("%s", text); });
-    imgui.set_function("LabelText", [](const char* label, const char* text) { ImGui::LabelText(label, "%s", text); });
-    imgui.set_function("BulletText", [](const char* text) { ImGui::BulletText("%s", text); });
+    imgui.set_function("Text", [](std::string_view text) { ImGui::Text("%s", text.data()); });
+    imgui.set_function("TextColored", [](float colr, float colg, float colb, float cola, std::string_view text) { ImGui::TextColored({colr, colg, colb, cola}, "%s", text.data()); });
+    imgui.set_function("TextDisabled", [](std::string_view text) { ImGui::TextDisabled("%s", text.data()); });
+    imgui.set_function("TextWrapped", [](std::string_view text) { ImGui::TextWrapped("%s", text.data()); });
+    imgui.set_function("LabelText", [](std::string_view label, std::string_view text) { ImGui::LabelText(label.data(), "%s", text.data()); });
+    imgui.set_function("BulletText", [](std::string_view text) { ImGui::BulletText("%s", text.data()); });
 }
