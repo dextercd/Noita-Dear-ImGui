a37058d422b06032afd50bcfdc9a5d9328670ff1
Dexter Castor Döpping <dexter.c.dopping@gmail.com>
2022-09-23 23:50:21 +0200

    Add TextUnformatted function

--- src/lua_features/imgui_text.cpp
+++ src/lua_features/imgui_text.cpp
@@ -1,10 +1,12 @@
 #include <tuple>
+#include <string_view>
 
 #include <sol/sol.hpp>
 #include <imgui.h>
 
 void add_imgui_text(sol::table& imgui)
 {
+    imgui.set_function("TextUnformatted", [](std::string_view text) { ImGui::TextUnformatted(text.data(), text.data() + text.size()); });
     imgui.set_function("Text", [](const char* text) { ImGui::Text("%s", text); });
     imgui.set_function("TextColored", [](float colr, float colg, float colb, float cola, const char* text) { ImGui::TextColored({colr, colg, colb, cola}, "%s", text); });
     imgui.set_function("TextDisabled", [](const char* text) { ImGui::TextDisabled("%s", text); });
