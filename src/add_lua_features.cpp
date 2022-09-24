46e5aeeed4552f63c62f30f31ff0a9dc0b91e101
Dexter Castor Döpping <dexter.c.dopping@gmail.com>
2022-09-24 23:02:21 +0200

    Add list clipper bindings

--- src/add_lua_features.cpp
+++ src/add_lua_features.cpp
@@ -18,6 +18,7 @@ void add_imgui_font(sol::table&);
 void add_imgui_gui_io(sol::table&);
 void add_imgui_keyboard_input(sol::table&);
 void add_imgui_layout(sol::table&);
+void add_imgui_list_clipper(sol::table&);
 void add_imgui_menu(sol::table&);
 void add_imgui_mouse_input(sol::table&);
 void add_imgui_param_stacks(sol::table&);
@@ -59,6 +60,7 @@ sol::table load_imgui(sol::this_state s, sol::table load_params)
     add_imgui_gui_io(imgui);
     add_imgui_keyboard_input(imgui);
     add_imgui_layout(imgui);
+    add_imgui_list_clipper(imgui);
     add_imgui_menu(imgui);
     add_imgui_mouse_input(imgui);
     add_imgui_param_stacks(imgui);
---
+++ src/lua_features/imgui_list_clipper.cpp
@@ -0,0 +1,21 @@
+#include <tuple>
+
+#include <sol/sol.hpp>
+#include <imgui.h>
+
+#include <sol/sol.hpp>
+#include <imgui.h>
+
+void add_imgui_list_clipper(sol::table& imgui)
+{
+    imgui.new_usertype<ImGuiListClipper>("ListClipper",
+        "Begin", sol::overload(
+            [](ImGuiListClipper& clipper, int items_count) { return clipper.Begin(items_count); },
+            sol::resolve<void(int, float)>(&ImGuiListClipper::Begin)),
+        "End", sol::resolve<void()>(&ImGuiListClipper::End),
+        "Step", sol::resolve<bool()>(&ImGuiListClipper::Step),
+        "ForceDisplayRangeByIndices", sol::resolve<void(int, int)>(&ImGuiListClipper::ForceDisplayRangeByIndices),
+        "DisplayStart", sol::readonly(&ImGuiListClipper::DisplayStart),
+        "DisplayEnd", sol::readonly(&ImGuiListClipper::DisplayEnd)
+    );
+}
