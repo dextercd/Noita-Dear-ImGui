94e7e112216623142e438b1f4bb31252919418c1
Dexter Castor Döpping <dexter.c.dopping@gmail.com>
2022-12-10 20:20:41 +0100

    Allow drag/drop across Lua context for string/number type

--- src/lua_features/imgui_drag_and_drop.cpp
+++ src/lua_features/imgui_drag_and_drop.cpp
@@ -1,7 +1,18 @@
 #include <sol/sol.hpp>
 #include <imgui.h>
+#include <iostream>
+
+enum class drag_drop_type {
+    string,
+    number,
+    object
+};
 
 using drag_drop_identifier = unsigned int;
+
+namespace {
+std::string string_data;
+}
 
 bool SetDragDropPayload(sol::this_state s, const char* type, sol::object payload, ImGuiCond cond = 0)
 {
@@ -10,28 +21,86 @@ bool SetDragDropPayload(sol::this_state s, const char* type, sol::object payload
     sol::state_view lua{s};
     auto globals = lua.globals();
 
-    bool ret = ImGui::SetDragDropPayload(type, &drag_drop_id, sizeof(drag_drop_id), cond);
-    if (ret) {
-        globals["__imgui_drag_drop_id"] = drag_drop_id++;
-        globals["__imgui_drag_drop_value"] = payload;
-    }
+    if (payload.is<std::string_view>()) {
+        auto data_type = drag_drop_type::string;
+        bool ret = ImGui::SetDragDropPayload(type, &data_type, sizeof(data_type), cond);
+        if (ret)
+            string_data = payload.as<std::string>();
 
-    return ret;
+        return ret;
+    } else if (payload.is<double>()) {
+        auto data_type = drag_drop_type::number;
+        double value = payload.as<double>();
+
+        char data[sizeof(data_type) + sizeof(value)];
+        std::memcpy(data, &data_type, sizeof(data_type));
+        std::memcpy(data + sizeof(data_type), &value, sizeof(value));
+
+        return ImGui::SetDragDropPayload(type, &data, sizeof(data), cond);
+    } else {
+        auto data_type = drag_drop_type::object;
+        auto value = drag_drop_id;
+
+        char data[sizeof(data_type) + sizeof(value)];
+        std::memcpy(data, &data_type, sizeof(data_type));
+        std::memcpy(data + sizeof(data_type), &value, sizeof(value));
+
+        bool ret = ImGui::SetDragDropPayload(type, &data, sizeof(data), cond);
+        if (ret) {
+            globals["__imgui_drag_drop_id"] = value;
+            globals["__imgui_drag_drop_value"] = payload;
+            ++drag_drop_id;
+        }
+
+        return ret;
+    }
 }
 
 sol::object value_from_payload(sol::state_view lua, const ImGuiPayload* payload)
 {
-    if (payload->DataSize != sizeof(drag_drop_identifier))
+    if (payload->DataSize < sizeof(drag_drop_type))
         return sol::lua_nil;
 
-    drag_drop_identifier drag_drop_id;
-    std::memcpy(&drag_drop_id, payload->Data, sizeof(drag_drop_identifier));
+    drag_drop_type data_type;
+    std::memcpy(&data_type, payload->Data, sizeof(data_type));
 
-    auto globals = lua.globals();
-    if (globals["__imgui_drag_drop_id"] != drag_drop_id)
-        return sol::lua_nil;
+    if (data_type == drag_drop_type::string &&
+        payload->DataSize == sizeof(data_type))
+    {
+        return sol::make_object(lua, string_data);
+    }
 
-    return globals["__imgui_drag_drop_value"];
+    if (data_type == drag_drop_type::number &&
+        payload->DataSize == sizeof(data_type) + sizeof(double))
+    {
+        double value;
+        std::memcpy(
+            &value,
+            (const char*)payload->Data + sizeof(data_type),
+            sizeof(value));
+
+        return sol::make_object(lua, value);
+    }
+
+    if (data_type == drag_drop_type::object &&
+        payload->DataSize == sizeof(data_type) + sizeof(drag_drop_identifier))
+    {
+        drag_drop_identifier drag_drop_id;
+        std::memcpy(
+            &drag_drop_id,
+            (const char*)payload->Data + sizeof(data_type),
+            sizeof(drag_drop_identifier));
+
+        auto globals = lua.globals();
+        if (globals["__imgui_drag_drop_id"] != drag_drop_id)
+            return sol::lua_nil;
+
+        return globals["__imgui_drag_drop_value"];
+    }
+
+    // Should never happen..
+    std::cerr << "Unknown drag drop type/size: " << (int)data_type << '/' << payload->DataSize << '\n';
+    return sol::lua_nil;
 }
 
 sol::object AcceptDragDropPayload(sol::this_state s, const char* type, ImGuiDragDropFlags flags = 0) {
