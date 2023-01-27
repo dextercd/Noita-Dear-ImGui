3ba9a26e30e50e7c6a38855478573cec69821651
Dexter Castor Döpping <dexter.c.dopping@gmail.com>
2023-01-27 02:41:01 +0100

    Fix missing Noita font characters

--- src/main.cpp
+++ src/main.cpp
@@ -46,10 +46,27 @@ void init_imgui_context()
 
     noita_imgui_style();
     auto font_path = mod_path + "/NoitaPixel.ttf";
-    io.Fonts->AddFontFromFileTTF(font_path.c_str(), 20);
-    io.Fonts->AddFontFromFileTTF(font_path.c_str(), 28);
-    io.Fonts->AddFontFromFileTTF(font_path.c_str(), 36);
+
+    ImVector<ImWchar> ranges;
+    ImFontGlyphRangesBuilder builder;
+    builder.AddChar(0x2013); // EN DASH
+    builder.AddChar(0x2014); // EM DASH
+    builder.AddChar(0x2019); // RIGHT SINGLE QUOTATION MARK
+    builder.AddChar(0x201c); // LEFT DOUBLE QUOTATION MARK
+    builder.AddChar(0x201d); // RIGHT DOUBLE QUOTATION MARK
+    builder.AddChar(0x201e); // DOUBLE LOW-9 QUOTATION MARK
+    builder.AddChar(0x2026); // HORIZONTAL ELLIPSIS
+    builder.AddChar(0x221e); // INFINITY
+    ImWchar latin_extended_a[]{0x100, 0x17f, 0};
+    builder.AddRanges(latin_extended_a);
+    builder.AddRanges(io.Fonts->GetGlyphRangesCyrillic());  // Includes default ranges
+    builder.BuildRanges(&ranges);
+
+    io.Fonts->AddFontFromFileTTF(font_path.c_str(), 20, nullptr, ranges.Data);
+    io.Fonts->AddFontFromFileTTF(font_path.c_str(), 28, nullptr, ranges.Data);
+    io.Fonts->AddFontFromFileTTF(font_path.c_str(), 36, nullptr, ranges.Data);
     io.Fonts->AddFontDefault();
+    io.Fonts->Build();  // Build with ranges in scope
 
     imgui_context_initialised = true;
 }
