df013edd23a1fe832ec3fdbf8c9ec1f075d85888
Dexter Castor Döpping <dexter.c.dopping@gmail.com>
2023-03-18 18:22:12 +0100

    Fix compilation error

--- src/lua_features/imgui_list_clipper.cpp
+++ src/lua_features/imgui_list_clipper.cpp
@@ -11,9 +11,9 @@ void add_imgui_list_clipper(sol::table& imgui)
     imgui.new_usertype<ImGuiListClipper>("ListClipper",
         "Begin", sol::overload(
             [](ImGuiListClipper& clipper, int items_count) { return clipper.Begin(items_count); },
-            sol::resolve<void(int, float)>(ImGuiListClipper::Begin)),
-        "End", sol::resolve<void()>(ImGuiListClipper::End),
-        "Step", sol::resolve<bool()>(ImGuiListClipper::Step),
+            sol::resolve<void(int, float)>(&ImGuiListClipper::Begin)),
+        "End", sol::resolve<void()>(&ImGuiListClipper::End),
+        "Step", sol::resolve<bool()>(&ImGuiListClipper::Step),
         "ForceDisplayRangeByIndices", sol::resolve<void(int, int)>(&ImGuiListClipper::ForceDisplayRangeByIndices),
         "DisplayStart", sol::readonly(&ImGuiListClipper::DisplayStart),
         "DisplayEnd", sol::readonly(&ImGuiListClipper::DisplayEnd)
