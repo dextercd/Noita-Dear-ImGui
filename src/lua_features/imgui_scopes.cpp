9b7edaa5e3881681991b8f8cf0f5a4de40d28ef0
Dexter Castor Döpping <dexter.c.dopping@gmail.com>
2023-03-18 18:19:20 +0100

    Fix PushID type error

--- src/lua_features/imgui_scopes.cpp
+++ src/lua_features/imgui_scopes.cpp
@@ -7,8 +7,8 @@ void add_imgui_scopes(sol::table& imgui)
 {
     imgui.set_function("PushID",
         sol::overload(
-            sol::overload<void(const char*)>(ImGui::PushID),
-            sol::overload<void(int)>(ImGui::PushID)));
+            sol::resolve<void(const char*)>(ImGui::PushID),
+            sol::resolve<void(int)>(ImGui::PushID)));
 
     imgui.set_function("PopID", sol::resolve<void()>(ImGui::PopID));
     imgui.set_function("GetID", sol::resolve<unsigned int(const char*)>(ImGui::GetID));
