ea6fac82dfa6068e908aee217c3a04e586c9c030
Dexter Castor Döpping <dexter.c.dopping@gmail.com>
2023-08-03 23:21:56 +0200

    Use faster TextUnformatted in bindings

--- src/lua_features/imgui_text.cpp
+++ src/lua_features/imgui_text.cpp
@@ -7,7 +7,7 @@
 void add_imgui_text(sol::table& imgui)
 {
     imgui.set_function("TextUnformatted", [](std::string_view text) -> void { ImGui::TextUnformatted(text.data(), text.data() + text.size()); });
-    imgui.set_function("Text", [](std::string_view text) -> void { ImGui::Text("%s", text.data()); });
+    imgui.set_function("Text", [](std::string_view text) -> void { ImGui::TextUnformatted(text.data()); });
     imgui.set_function("TextColored", [](float colr, float colg, float colb, float cola, std::string_view text) -> void { ImGui::TextColored({colr, colg, colb, cola}, "%s", text.data()); });
     imgui.set_function("TextDisabled", [](std::string_view text) -> void { ImGui::TextDisabled("%s", text.data()); });
     imgui.set_function("TextWrapped", [](std::string_view text) -> void { ImGui::TextWrapped("%s", text.data()); });
