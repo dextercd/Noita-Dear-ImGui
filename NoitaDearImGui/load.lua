3c0f21954b297e0bb0681d375fee89f88e7726fe
Dexter Castor Döpping <dexter.c.dopping@gmail.com>
2024-02-24 23:04:29 +0100

    Add VC++ redistributables to mod zip

--- NoitaDearImGui/load.lua
+++ NoitaDearImGui/load.lua
@@ -2,13 +2,13 @@
 -- if the standalone version is installed.
 
 local ffi = require("ffi")
+local C = ffi.C
 
 return function(path, name)
     local embedded_version = name ~= "imgui"
     if embedded_version and ModIsEnabled("NoitaDearImGui") then
         return nil
     end
-
 
     ffi.cdef([[
 
@@ -29,14 +29,17 @@ return function(path, name)
     );
     void imgui_signal_unpause();
 
+    int SetDllDirectoryA(const char* lpPathName);
+    unsigned long GetDllDirectoryA(unsigned long nBufferLength, char* lpBuffer);
     void* LoadLibraryA(const char*);
+
+    unsigned long GetLastError();
 
     int SDL_PollEvent(struct SDL_Event* event);
     void SDL_GL_SwapWindow(struct SDL_Window* window);
     struct lua_State* luaL_newstate();
 
     ]])
-
 
     local settings_prefix = name .. ".imgui_"
     if not embedded_version then
@@ -47,11 +50,22 @@ return function(path, name)
         return ModSettingGet(settings_prefix .. key) or false
     end
 
-
     local imgui_dll_path = path .. "/noita_dear_imgui.dll"
     if setting_get("build_type") == "debug" then
         imgui_dll_path = path .. "/Debug/noita_dear_imgui.dll"
     end
+
+    local old_dll_dir = nil
+    local old_dll_dir_buf = ffi.new("char[256]")
+    local old_dll_dir_sz = C.GetDllDirectoryA(ffi.sizeof(old_dll_dir_buf), old_dll_dir_buf)
+    if old_dll_dir_sz ~= 0 then
+        old_dll_dir = ffi.string(old_dll_dir_buf, old_dll_dir_sz)
+    end
+
+    -- We include the VC++ runtime libs in the mod directory so the user doesn't
+    -- have to install it themselves manually. This line makes the Windows
+    -- loader look inside this directory.
+    C.SetDllDirectoryA(imgui_dll_path:match(".*/"))
 
     -- LuaJIT frees the OS library handle when the `ffi.load` handle is garbage
     -- collected. We need to increase the library's reference count to prevent it
@@ -59,7 +73,15 @@ return function(path, name)
     --
     -- Without this, Noita crashes in JIT-ed Lua code when you use the "New Game"
     -- option, probably because some Lua states are preserved across new runs.
-    assert(ffi.C.LoadLibraryA(imgui_dll_path) ~= nil)
+    if C.LoadLibraryA(imgui_dll_path) == nil then
+        local err = C.GetLastError()
+        error("ImGui load error: " .. err)
+    end
+
+    -- Restore to previous dll directory
+    -- old_dll_dir may be nil, in which case SetDllDirectoryA restores the
+    -- default search order, and that should be OK.
+    C.SetDllDirectoryA(old_dll_dir)
 
     local imgui_dll = ffi.load(imgui_dll_path)
     local sdl = ffi.load("SDL2.dll")
@@ -70,7 +92,7 @@ return function(path, name)
         name,
         sdl.SDL_PollEvent,
         sdl.SDL_GL_SwapWindow,
-        ffi.C.luaL_newstate
+        C.luaL_newstate
     )
 
     return {
